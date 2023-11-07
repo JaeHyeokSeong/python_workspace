1c2db3684c3f2531151d146c0ca49b2702f52009
JaeHyeokSeong <seong5763@gmail.com>
2023-11-08 00:40:26 +1100

    create sign-in

--- flask_practise/project/real_estate/main.py
+++ flask_practise/project/real_estate/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/sign-in/', methods=['GET', 'POST'])
+def sign_in():
+    if request.method == 'GET':
+        return render_template('sign-in.html')
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        print(f'username: {username}, password: {password}')
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
