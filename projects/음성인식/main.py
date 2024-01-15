b22a59a2f879d4cecb44a30bcd50938127b708ae
JaeHyeokSeong <seong5763@gmail.com>
2024-01-16 05:03:35 +1100

    음성인식 프로그램

--- projects/음성인식/main.py
+++ projects/음성인식/main.py
@@ -1,3 +1,17 @@
-# 음성 인식 프로그램 입니다.
+import speech_recognition as sr
 
-print('hello world')
+if __name__ == '__main__':
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('지금 말해주세요:')
+        # 총 4초 동안 음성을 인식 합니다.
+        speech = recognizer.listen(source, phrase_time_limit=7)
+
+    try:
+        audio = recognizer.recognize_google(speech, language='ko')
+        print(f'인식 되어진 결과: {audio}')
+    except sr.UnknownValueError:
+        print('인식을 하지 못했습니다.')
+    except sr.RequestError as e:
+        print(f'Request error: {e}')
